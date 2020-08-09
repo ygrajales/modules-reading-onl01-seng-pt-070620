87a4db621762cd616d99c65b80213774aaa6b51e
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-08-09 22:45:22 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,11 +1,11 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
 
 class Dancer
   
-  include Dance 
-  
-  extend MetaDancing
+  extend FancyDance::ClassMethods 
+  include FanceDance::InstanceMethods
   
   attr_accessor :name 
   
